6020f75f724acfb86c6fd918b00f8cb70b43175d
Sergey Psekha <pss@ciklum.com>
2012-12-07 13:40:12 +0200

    projects added;

---
+++ MapReduce.Tests/Integration.cs
@@ -0,0 +1,27 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="Integration.cs" company="">
+// TODO: Update copyright text.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace MapReduce.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using NUnit.Framework;
+
+    /// <summary>
+    /// TODO: Update summary.
+    /// </summary>
+    [TestFixture]
+    public class Integration
+    {
+        [Test]
+        public void Should_connects_to_the_server()
+        { 
+
+        }
+    }
+}

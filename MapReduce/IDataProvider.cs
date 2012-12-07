12fec9e9b677faab6ca5b14fa7323c3ef889f85b
Sergey Psekha <pss@ciklum.com>
2012-12-07 15:09:04 +0200

    mp-0001: references added;

---
+++ MapReduce/IDataProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MapReduce
+{    
+    public interface IDataProvider
+    {
+        void Connect();
+        void Disconnect();
+    }
+}
---
+++ MapReduce/MongoDataProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using MongoDB.Driver;
+
+namespace MapReduce
+{
+    public class MongoDataProvider : IDataProvider
+    {
+        private const string ConnectionString = "mongodb://localhost/MapReduce";
+
+        private MongoDatabase database;
+
+        public void Connect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Disconnect()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
